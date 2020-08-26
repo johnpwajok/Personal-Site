95896acefde0cc70b4e03cec3f597acf91df062b
John Pwajok <johnpwajok97@hotmail.com>
2020-08-26 16:28:39 +0100

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import NavigationBar from "./components/NavigationBar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Landing from "./components/Landing";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.connecToServer = this.connecToServer.bind(this);
+  }
+  connecToServer() {
+    fetch("/");
+  }
+  componentDidMount() {
+    this.connecToServer();
+  }
+  render() {
+    return (
+      <Router>
+        {" "}
+        <div className="container">
+          {" "}
+          <NavigationBar /> <Route exact path="/" component={Landing} />{" "}
+        </div>{" "}
+      </Router>
+    );
+  }
+}
+export default App;
---
+++ client/src/components/NavigationBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class NavigationBar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Navigation Bar</h1>
+      </div>
+    );
+  }
+}
