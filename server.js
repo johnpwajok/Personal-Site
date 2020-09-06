b0d7e8697bf6498558ea4e3e9274cf1b63528008
John Pwajok <johnpwajok97@hotmail.com>
2020-09-06 11:26:52 +0100

    HTTPS change

--- server.js
+++ server.js
@@ -3,18 +3,17 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 5000;
 //Added for https
-const http = require("http");
 const enforce = require("express-sslify");
+
+//HTTPS CODE
+// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
+// a load balancer (e.g. Heroku). See further comments below
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+//End https section
 //Static file declaration
 app.use(express.static(path.join(__dirname, "client/build")));
 //production mode
 if (process.env.NODE_ENV === "production") {
-  //HTTPS CODE
-  // Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
-  // a load balancer (e.g. Heroku). See further comments below
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
-  //End https section
-
   app.use(express.static(path.join(__dirname, "client/build"))); //  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
   //build mode
   app.get("*", (req, res) => {
