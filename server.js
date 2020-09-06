693886435fe50008d03a38eeef2272bca690f921
John Pwajok <johnpwajok97@hotmail.com>
2020-09-06 10:55:27 +0100

    testing https

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 5000;
+//Added for https
+const http = require("http");
+const enforce = require("express-sslify");
 //Static file declaration
 app.use(express.static(path.join(__dirname, "client/build")));
 //production mode
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build"))); //  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
+  //app.use(express.static(path.join(__dirname, "client/build"))); //  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
+
+  //HTTPS CODE
+  // Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
+  // a load balancer (e.g. Heroku). See further comments below
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   //build mode
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/client/public/index.html"));
